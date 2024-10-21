7c850caa22ca1244cd466c614219c751b8c9cb02
DiogomgTrindade <diogovsky1904@gmail.com>
2024-10-21 15:10:36 +0100

    App Dividir a Conta

--- MauiControls/App.xaml.cs
+++ MauiControls/App.xaml.cs
@@ -8,7 +8,7 @@ namespace MauiControls
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new  MauiResources());
+            MainPage = new NavigationPage(new DividirAConta());
         }
     }
 }

88eda15415614b9f51b0e0a9a1f2e31f6ec4d78f
DiogomgTrindade <diogovsky1904@gmail.com>
2024-10-21 12:25:45 +0100

    Jogo da Forca

--- MauiControls/App.xaml.cs
+++ MauiControls/App.xaml.cs
@@ -8,7 +8,7 @@ namespace MauiControls
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new  NotifyBinding());
+            MainPage = new NavigationPage(new  JogoDaForca());
         }
     }
 }
